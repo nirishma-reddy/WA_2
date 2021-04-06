048c8b2af622e2ed2882f772e0ae9cee030e5eb1
nirishma-reddy <81669123+nirishma-reddy@users.noreply.github.com>
2021-04-06 03:19:17 -0700

    Add files via upload

---
+++ LargestElement_array.java
@@ -0,0 +1,14 @@
+public class LargestElement_array
+{
+public static void main(String[] args)
+{
+int[] arr = new int[]{45,647,65,84,65};
+int max= arr[0];
+for (int i=0;i<arr.length;i++)
+{
+if(arr[i]>max)
+max = arr[i];
+}
+System.out.println("Largest element present in given array:"+max);
+}
+}
